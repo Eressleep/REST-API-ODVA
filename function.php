07ef459278b889e299a38666cdbd4c55c3824089
Alexsander Semenov <xsander.semenov.ser@bk.ru>
2020-11-30 15:47:46 +0300

    post ref

---
+++ function.php
@@ -0,0 +1,83 @@
+<?php
+
+function link_youtube($object)
+{
+	preg_match_all('#(?:https?|ftp)://[^\s\,]+#i', get_post_field('video_in_post', $object['id']), $matches);
+	return str_replace('"', '', $matches[0])[0];
+}
+function categories($object)
+{
+	return  wp_get_post_categories($object['id']);
+}
+function content($object)
+{
+	return $object['content']['rendered'];
+}
+function excerpt($object)
+{
+	return $object['excerpt']['rendered'];
+}
+function hashtag($object)
+{
+	$arr = [];
+	foreach (get_the_tags($object['id']) as $tags)
+	{
+		$arr[] = "#".$tags->name;
+
+	}
+	return $arr;
+}
+function img($object)
+{
+	return get_the_post_thumbnail_url($object['id']);
+}
+function related($object){
+	$tags_post = get_the_tags( $object['id']);
+	$tags_array = array();
+
+	foreach ($tags_post as $tags_post_item) {
+		array_push($tags_array, $tags_post_item->term_id);
+	}
+	$args_on_this_topic = array(
+		'post_status' => 'publish',
+		'posts_per_page' => 3,
+		'tag__in' => $tags_array,
+		'post__not_in' =>  array( $object['id']),
+	);
+	$wp_query_on_this_topic = new WP_Query($args_on_this_topic);
+	$rel_posts = [];
+	foreach ($wp_query_on_this_topic->posts as $post)
+	{
+		preg_match_all('#(?:https?|ftp)://[^\s\,]+#i', get_post_field('video_in_post', $post->ID), $match_video);
+		str_replace('"', '', $match_video[0]);
+		$post_hashtag = [];
+		foreach (get_the_tags($post->ID) as $tags)
+		{
+			$post_hashtag[] = "#".$tags->name;
+
+		}
+		$rel_posts[] = [
+			'ID' => $post->ID,
+			'img' => img($post->ID),
+			'title' => $post->post_title,
+			'date' => $post->post_date,
+			'content' => $post->post_content,
+			'excerpt' => $post->post_excerpt,
+			'views' => get_field('views', $post->ID),
+			'comment_count' => $post->comment_count,
+			'video' => $match_video[0],
+			'post_hashtag' => $post_hashtag
+
+		];
+	}
+	return $rel_posts;
+}
+function title($object){
+	return $object['title']['rendered'];
+}
+function views($object){
+	return get_field('views',$object['id']);
+}
+function comments_count($object){
+	return wp_count_comments($object['id'])->total_comments;
+}
--- plugin.php
+++ plugin.php
@@ -10,6 +10,7 @@
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  **/
 define('SHORTINIT',1);
+require dirname( __FILE__ ) .'/function.php';
 require dirname( __FILE__ ) .'/posts/posts.php';
-
+require dirname( __FILE__ ) .'/tagsToday/tags.php';
 odvaPost();
